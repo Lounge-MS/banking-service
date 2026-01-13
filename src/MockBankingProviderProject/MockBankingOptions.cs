6b2aee2adc1dc0a8ed17a52de8d5a70a18d31878
hhhannahmmmontana <rubakhin.edu@niuitmo.ru>
2026-01-13 21:54:12 +0300

    delete: removed rollbacks in options and settings in mock

--- src/MockBankingProviderProject/MockBankingOptions.cs
+++ src/MockBankingProviderProject/MockBankingOptions.cs
@@ -4,8 +4,6 @@ public record MockBankingOptions
 {
     public bool AllowPayments { get; set; } = true;
 
-    public bool AllowRollbacks { get; set; } = true;
-
     public bool RequireUrlVisit { get; set; } = true;
 
     public int DelayMs { get; set; } = 0;
